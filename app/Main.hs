63a33edae57b7d354d46d8c45831859824d240a4
yosukeueda33 <iwalkedalongthewalllikaeninjaa@gmail.com>
2026-06-18 01:08:41 +0900

    Add echo.

--- app/Main.hs
+++ app/Main.hs
@@ -10,18 +10,78 @@ import Ivory.Compile.C.CmdlineFrontend
 printf :: Def ('[IString] ':-> Sint32)
 printf = importProc "printf" "stdio.h"
 
+txBuffer :: MemArea (Array 4 (Stored Uint8))
+txBuffer = area "txBuffer" . Just . iarray $ replicate 4 izeroval
+
+txBufferHead :: MemArea ('Stored Uint8)
+txBufferHead = area "txBufferHead" $ Just (ival 0)
+
+txBufferTail :: MemArea ('Stored Uint8)
+txBufferTail = area "txBufferTail" $ Just (ival 0)
+
+
+-- setMeter :: Def ('[Uint8, Uint8] ':-> ())
+-- setMeter = importProc "set_meter" "infra.h"
+
+-- setLight :: Def ('[Uint8, Uint8] ':-> ())
+-- setLight = importProc "set_light" "infra.h"
+
 hello :: Def ('[] ':-> ())
 hello = proc "hello" $ body $ do
   call_ printf "Hello, world\n"
   retVoid
+
+pushTxBuffer :: Def ('[Uint8] ':-> IBool)
+pushTxBuffer = proc "pushTxBuffer" $ \x -> body $ do
+  h <- deref (addrOf txBufferHead)
+  t <- deref (addrOf txBufferTail)
+
+  let next = (h + 1) .% 4
+
+  ifte_ (next ==? t)
+    ( do
+        ret false
+    )
+    ( do
+        store (addrOf txBuffer ! toIx h) x
+        store (addrOf txBufferHead) next
+        ret true
+    )
+
+popTxBuffer :: Def ('[Ref s ('Stored Uint8)] ':-> IBool)
+popTxBuffer = proc "popTxBuffer" $ \out -> body $ do
+  h <- deref (addrOf txBufferHead)
+  t <- deref (addrOf txBufferTail)
+
+  ifte_ (h ==? t)
+    ( do
+        ret false
+    )
+    ( do
+        x <- deref (addrOf txBuffer ! toIx t)
+        store out x
+        store (addrOf txBufferTail) ((t + 1) .% 4)
+        ret true
+    )
 
 helloModule :: Module
 helloModule = package "HelloWorld" $ do
   incl printf
   incl hello
 
+communicateModule :: Module
+communicateModule = package "Communicate" $ do
+  -- incl parseByte
+  incl pushTxBuffer
+  incl popTxBuffer
+  defMemArea txBuffer
+  defMemArea txBufferHead
+  defMemArea txBufferTail
+  -- incl setMeter
+  -- incl setLight
+
 main :: IO ()
 main =
-  runCompiler [helloModule] [] initialOpts
+  runCompiler [helloModule, communicateModule] [] initialOpts
     { outDir = Just "generated"
     }
---
+++ echo2313.c
@@ -0,0 +1,152 @@
+/*
+ * echo2313.c
+ *
+ * ATtiny2313 UART echo-back using a 4-byte TX ring buffer.
+ *
+ * F_CPU: 10 MHz
+ * Baud : 9600 bps
+ *
+ * RX  : PD0
+ * TX  : PD1
+ */
+
+#ifndef F_CPU
+#define F_CPU 10000000UL
+#endif
+
+#define BAUD 9600UL
+#define UBRR_VALUE ((F_CPU / (16UL * BAUD)) - 1UL)
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/sleep.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include "generated/Communicate.h"
+
+/* -----------------------------
+ * TX ring buffer
+ * 容量4、ただし1スロット空ける方式なので実効容量は3バイト
+ * ----------------------------- */
+
+// #define TX_BUFFER_SIZE 4
+
+// static volatile uint8_t txBuffer[TX_BUFFER_SIZE];
+// static volatile uint8_t txBufferHead = 0;
+// static volatile uint8_t txBufferTail = 0;
+
+/* -----------------------------
+ * USART
+ * ----------------------------- */
+
+static void uart_init(void)
+{
+    uint16_t ubrr = (uint16_t)UBRR_VALUE;
+
+    UBRRH = (uint8_t)(ubrr >> 8);
+    UBRRL = (uint8_t)(ubrr & 0xFF);
+
+    /*
+     * 8N1:
+     * - asynchronous
+     * - no parity
+     * - 1 stop bit
+     * - 8 data bits
+     */
+    UCSRC = (uint8_t)(_BV(UCSZ1) | _BV(UCSZ0));
+
+    /*
+     * Enable RX, TX, RX complete interrupt.
+     * UDRE interrupt is enabled only when TX buffer has data.
+     */
+    UCSRB = (uint8_t)(_BV(RXEN) | _BV(TXEN) | _BV(RXCIE));
+}
+
+static void uart_enable_udre_interrupt(void)
+{
+    UCSRB |= _BV(UDRIE);
+}
+
+static void uart_disable_udre_interrupt(void)
+{
+    UCSRB &= (uint8_t)~_BV(UDRIE);
+}
+
+/* -----------------------------
+ * Interrupt vectors
+ * avr-libcのデバイス定義差分吸収
+ * ----------------------------- */
+
+#if defined(USART_RX_vect)
+#define UART_RX_VECTOR USART_RX_vect
+#elif defined(USART0_RX_vect)
+#define UART_RX_VECTOR USART0_RX_vect
+#elif defined(USART_RXC_vect)
+#define UART_RX_VECTOR USART_RXC_vect
+#else
+#error "No USART RX vector name found for this device"
+#endif
+
+#if defined(USART_UDRE_vect)
+#define UART_UDRE_VECTOR USART_UDRE_vect
+#elif defined(USART0_UDRE_vect)
+#define UART_UDRE_VECTOR USART0_UDRE_vect
+#elif defined(USART_DRE_vect)
+#define UART_UDRE_VECTOR USART_DRE_vect
+#else
+#error "No USART UDRE vector name found for this device"
+#endif
+
+/*
+ * 1バイト受信したら、そのままTXバッファへ積む。
+ * バッファ満杯なら捨てる。
+ */
+ISR(UART_RX_VECTOR)
+{
+    uint8_t x = UDR;
+
+    if (pushTxBuffer(x)) {
+        uart_enable_udre_interrupt();
+    }
+}
+
+/*
+ * 送信データレジスタが空になったら、TXバッファから1バイト送る。
+ * 空ならUDRE割り込みを止める。
+ */
+ISR(UART_UDRE_VECTOR)
+{
+    uint8_t x;
+
+    if (popTxBuffer(&x)) {
+        UDR = x;
+
+        /*
+         * 送った結果、空になったなら次のUDRE割り込みは不要。
+         * UDRに書いたバイト自体はこの後ハードウェアが送信する。
+         */
+        if (txBufferHead == txBufferTail) {
+            uart_disable_udre_interrupt();
+        }
+    } else {
+        uart_disable_udre_interrupt();
+    }
+}
+
+/* -----------------------------
+ * main
+ * ----------------------------- */
+
+int main(void)
+{
+    uart_init();
+
+    set_sleep_mode(SLEEP_MODE_IDLE);
+    sleep_enable();
+
+    sei();
+
+    for (;;) {
+        sleep_cpu();
+    }
+}
